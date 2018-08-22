792b1b4bf3825b13dbeb259a22ea9361770b28fb
khush kaushal <khushkaushal62@yahoo.com>
2018-08-21 18:59:56 -0700

    A basic UI and some C# code

---
+++ Aisle/Aisle/Aisle/Views/HomePage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Aisle.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class HomePage : ContentPage
+	{
+
+        private readonly List<string> _names = new List<string>
+        {
+            "hulu","lulu","bholu","Jhulu","chullu","mulu"
+        };
+
+		public HomePage ()
+		{
+			InitializeComponent ();
+
+            Item.ItemsSource = _names;
+		}
+
+        private void ItemSearchBar_SearchButtonPressed(object sender, EventArgs e)
+        {
+            string keyword = ItemSearchBar.Text;
+
+           IEnumerable<string> searchResult = _names.Where(name => name.Contains(keyword));
+
+            Item.ItemsSource = searchResult;
+
+        }
+    }
+}
